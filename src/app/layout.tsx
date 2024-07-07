2875da51784b3c76396adcda97284cb6b4b75122
Anna Viklund <annamariaviklund@gmail.com>
2024-07-07 21:47:42 +0200

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/png" href="/ami-logo.png" />
       <body className={inter.className}>
         <TopBar />
         <main>{children}</main>
